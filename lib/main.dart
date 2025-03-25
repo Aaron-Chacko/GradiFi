f76f07a36a1bd7d11f4980ceb3b8bddb5db37716
Rochan-Arya <rochanarya@gmail.com>
2025-03-25 19:58:12 +0530

    feat: add nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/home_page.dart'; // Import your HomePage
+import 'screens/home_page.dart'; 
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomePage(), // Use HomePage instead of MyHomePage
+      home: const HomePage(), 
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+List<IconData> navIcons = [
+  Icons.home_filled,
+  Icons.cloud_upload_outlined,
+  Icons.document_scanner_outlined,
+];
+
+List<String> navTitles = ["Home", "Upload", "Scan"];
+int selectedIndex = 0;
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +53,7 @@ class HomePage extends StatelessWidget {
           const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: Text(
-              'Dashboard', //---------------dashboard------------------
+              'Dashboard',
               style: TextStyle(
                 fontSize: 22,
                 fontWeight: FontWeight.bold,
@@ -48,9 +62,7 @@ class HomePage extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 10),
-
           Padding(
-            //-----------------card---------------
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Card(
               color: Colors.grey[900],
@@ -105,57 +117,122 @@ Pass Percentage: 84.2%''',
               ),
             ),
           ),
-          const SizedBox(height: 30), // Spacing between card and title
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Text(
-              'Files Uploaded', //------------------files uploaded---------------------
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+          const SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                const Text(
+                  'Files Uploaded',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(width: 100),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Add Files",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color(0xFFCEFF02),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Card(
+              color: Colors.grey[900],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              elevation: 4,
+              child: const Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'List',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text('''Syllabus.pdf
+Unit Content.pdf
+Question Paper.pdf
+Answer Key.pdf''', style: TextStyle(fontSize: 16, color: Colors.white70)),
+                  ],
+                ),
               ),
             ),
           ),
-
-          const SizedBox(height: 10),
-
-          Padding( //----------------------second card-----------------------
-  padding: const EdgeInsets.symmetric(horizontal: 20),
-  child: Card(
-    color: Colors.grey[900],
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(15),
-    ),
-    elevation: 4,
-    child: Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          Text(
-            'List',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-          SizedBox(height: 20),
-          Text(
-            '''Syllabus.pdf
-Unit Content.pdf
-Question Paper.pdf
-Answer Key.pdf''',
-            style: TextStyle(fontSize: 16, color: Colors.white70),
-          ),
+          const Spacer(),
+          Align(alignment: Alignment.bottomCenter, child: _navBar()),
         ],
       ),
-    ),
-  ),
-),
+    );
+  }
 
-        ],
+  Widget _navBar() {
+    return Container(
+      height: 65,
+      margin: EdgeInsets.only(right: 24, left: 24, bottom: 24),
+      decoration: BoxDecoration(
+        color: Color(0XFF1C1C1C),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:
+            navIcons.map((icon) {
+              int index = navIcons.indexOf(icon);
+              bool isSelected = selectedIndex == index;
+              return Material(
+                color: Colors.transparent,
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.only(
+                          top: 15,
+                          bottom: 0,
+                          left: 35,
+                          right: 35,
+                        ),
+                        child: Icon(
+                          icon,
+                          color: isSelected ? Color(0xFFCEFF02) : Colors.white,
+                        ),
+                      ),
+                      Text(
+                        navTitles[index],
+                        style: TextStyle(
+                          color: isSelected ? Color(0xFFCEFF02) : Colors.white,
+                          fontSize: 10,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }).toList(),
       ),
     );
   }
